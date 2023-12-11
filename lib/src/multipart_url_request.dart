c8d77275ef62f67a22c2df49c7db1b6eea816881
Victor Carreras <victor.carreras-jimenez@capgemini.com>
2023-12-11 08:47:18 +0100

    Fixed more warnings

--- lib/src/multipart_url_request.dart
+++ lib/src/multipart_url_request.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import '../network_wrapper.dart';
-import 'method_request.dart';
 
 /// This class represents all the fields that can be required by multipart request of a network call,
 /// in such a way that it encapsulates the implementation for the rest of the projects.
@@ -10,13 +9,12 @@ class MultipartUrlRequest extends UrlRequest {
   Map<String, dynamic>? fieldsMultipart;
   MethodRequest method;
 
-  MultipartUrlRequest(Uri url, this.method,
-      {Map<String, String>? headers,
-      Object? body,
-      Encoding? encoding,
+  MultipartUrlRequest(super.url, this.method,
+      {super.headers,
+      super.body,
+      super.encoding,
       this.filesMultipart,
-      this.fieldsMultipart})
-      : super(url, headers: headers, body: body, encoding: encoding);
+      this.fieldsMultipart});
 
   MultipartUrlRequest.fromStringUrl(String uriString, this.method,
       {Map<String, String>? headers,
